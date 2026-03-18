67e212c6fda2a92a027c1359d04643687b10deed
os85907309-glitch <os85907309@gmail.com>
2026-03-18 15:27:13 +0900

    tired
    ㅠㅠ

--- 20260318/ex11.js
+++ 20260318/ex11.js
@@ -1,1 +1,21 @@
-console.log("test");
+document.getElementById("chk").onclick = function () {
+    console.log("test");
+    const test = document.getElementById('num').value;
+    console.log("test = " + test);
+
+    if(test == ''){
+        console.log("test를 입력해주세요");
+        document.getElementById('print').textContent = "test를 입력해주세요";
+        return;
+    }
+
+    if (test < 3) {
+        console.log(test + "는 3보다 작다");
+    } 
+    else if (test == 3) {
+        console.log(test + "는 3과 같다");
+    }
+    else {
+        console.log(test + "는 3보다 크다");
+    }
+}
